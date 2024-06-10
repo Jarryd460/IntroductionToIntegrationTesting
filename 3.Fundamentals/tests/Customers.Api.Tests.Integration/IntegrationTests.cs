2587bccedb1d46ccbec99909f20e2ca4d415dc18
Jarryd Deane <jarryd.deane@gmail.com>
2024-06-10 17:49:03 +0200

    Write first integration test
    Write first unit test by connecting to customers Api and getting a customer by their Guid Id

---
+++ 3.Fundamentals/tests/Customers.Api.Tests.Integration/IntegrationTests.cs
@@ -0,0 +1,19 @@
+using System.Net;
+
+namespace Customers.Api.Tests.Integration;
+
+public sealed class IntegrationTests
+{
+    [Fact]
+    public async Task Test()
+    {
+        var httpClient = new HttpClient()
+        {
+            BaseAddress = new Uri("https://localhost:5001")
+        };
+
+        var response = await httpClient.GetAsync($"customers/{ Guid.NewGuid() }");
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
