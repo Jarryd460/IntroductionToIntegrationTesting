5d5efbafad3e723652425997d2b98fd158f305ae
Jarryd Deane <jarryd.deane@gmail.com>
2024-06-10 17:56:46 +0200

    Rename and structure test class and test
    Appropriately name the test class and the test and arrange the unit test using arrange, act, assert (AAA)

--- 3.Fundamentals/tests/Customers.Api.Tests.Integration/IntegrationTests.cs
+++ 3.Fundamentals/tests/Customers.Api.Tests.Integration/IntegrationTests.cs
@@ -2,18 +2,21 @@ using System.Net;
 
 namespace Customers.Api.Tests.Integration;
 
-public sealed class IntegrationTests
+public sealed class CustomerControllerTests
 {
     [Fact]
-    public async Task Test()
+    public async Task Get_Should_ReturnNotFound_When_CustomerDoesNotExist()
     {
+        // Arrange
         var httpClient = new HttpClient()
         {
             BaseAddress = new Uri("https://localhost:5001")
         };
 
+        // Act
         var response = await httpClient.GetAsync($"customers/{ Guid.NewGuid() }");
 
+        // Assert
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
     }
 }
